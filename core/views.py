f8399d934f5e18ff616dca41feb3e7b7633e10d7
Harshit Sharma <harshu729@gmail.com>
2021-02-16 17:48:03 +0530

    YY legend

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+from .models import Member
+import json
+app_name = "core"
+
+# Create your views here.
+
+def jsonitems(request,dept):
+    coreinfo = serializers.serialize("json",Member.objects.all().filter(deptName__icontains=dept).order_by('posnum','name'))
+    coreinfo = json.loads(coreinfo)
+    return JsonResponse(coreinfo,safe=False)
+
+def core(request):
+    return render(request, 'core/core.html')
