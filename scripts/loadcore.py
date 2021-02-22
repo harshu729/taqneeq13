a23ba7bc553000a4cf52f353e47fe5f9db6aceb6
Parv Dave <parvdave29@gmail.com>
2021-02-22 20:26:48 +0530

    Added databases

--- scripts/loadcore.py
+++ scripts/loadcore.py
@@ -2,14 +2,16 @@ import pandas as pd
 from core.models import Member
 def run():
     data = pd.read_csv('memberdeets.csv')
-    columns = [i for i in data][1:]
-    
-    for i in range(80):
-        name=data[columns[0]][i]+' '+data[columns[1]][i]
-        position,deptName,desc=data[columns[2]][i],data[columns[3]][i],data[columns[4]][i]
-        if position in ["Chairperson","Head"]:
-            m = Member(name=name,position=position,deptName=deptName,posnum=1,desc=desc)
+    columns = [i for i in data][0:]
+    print(columns)
+    for i in range(len(data)-1):
+        name=data[columns[0]][i]
+        position,deptName=data[columns[1]][i],data[columns[2]][i]
+        gifurl = data[columns[4]][i]
+        if(position in ['Head','Chairperson']):
+            m = Member(name=name,position=position,deptName=deptName,posnum=1,gifurl=gifurl)
+            m.save()
         else:
-            m = Member(name=name,position=position,deptName=deptName,posnum=2,desc=desc)
-        m.save()
+            m = Member(name=name,position=position,deptName=deptName,posnum=2,gifurl=gifurl)
+            m.save()
 
