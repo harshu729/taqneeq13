9559c62b69503c10acbdc9eb6fdab23446f66ff9
Harshit Sharma <harshu729@gmail.com>
2021-02-25 03:39:43 +0530

    TQfinalpush

---
+++ events/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+app_name = "events"
+
+urlpatterns = [
+    path('',views.ourevents,name='ourevents'),
+    path('liveupdates/',views.liveupdates,name='liveupdates'),
+    path('<str:dept>/',views.eachevent,name="event-view")
+]
