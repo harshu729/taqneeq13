ffad243cbd9ee63c8203c30fa54d3e5e124c4e97
Harshit Sharma <harshu729@gmail.com>
2021-02-25 03:36:43 +0530

    YYdelete

--- events/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def ourevents(request):
-    return render(request,'events/events.html',{})
-    
--- sponsors/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def sponsors(request):
-    return render(request,'sponsors/sponsors.html',{})
